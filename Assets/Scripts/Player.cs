f015fee1a3ffb68d37b7e4b9db4d638f307b422f
jb.Park <sumeing.vogi@gmail.com>
2024-03-06 18:07:06 +0900

    feat : 캐릭터 이동, 폰트추가, 텍스쳐수정

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Component")]
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] SpriteRenderer render;
+    [SerializeField] Animator animator;
+
+    [Header("Move")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float breakPower;
+    [SerializeField] float maxXSpeed;
+    [SerializeField] float maxYSpeed;
+
+    [Header("Jump")]
+    [SerializeField] float jumpSpeed;
+    [SerializeField] LayerMask groundCheckLayer;
+
+    private Vector2 moveDir;
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        if ( moveDir.x < 0 && rigid.velocity.x > -maxXSpeed )
+        {
+            rigid.AddForce(Vector2.right * moveSpeed * moveDir.x);
+        }
+        else if ( moveDir.x > 0 && rigid.velocity.x < maxXSpeed )
+        {
+            rigid.AddForce(Vector2.right * moveSpeed * moveDir.x);
+        }
+        else if ( moveDir.x == 0 && rigid.velocity.x > 0.1f )
+        {
+            rigid.AddForce(Vector2.left * breakPower);
+        }
+        else if ( moveDir.x == 0 && rigid.velocity.x < -0.1f )
+        {
+            rigid.AddForce(Vector2.right * breakPower);
+        }
+
+        if ( rigid.velocity.y < -maxYSpeed )
+        {
+            Vector2 velocity = rigid.velocity;
+            velocity.y = -maxYSpeed;
+            rigid.velocity = velocity;
+        }
+
+        animator.SetFloat("YSpeed", rigid.velocity.y);
+    }
+
+    private void Jump()
+    {
+        Vector2 velocity = rigid.velocity;
+        velocity.y = jumpSpeed;
+        rigid.velocity = velocity;
+    }
+
+    private void OnMove( InputValue value )
+    {
+        moveDir = value.Get<Vector2>();
+        if ( moveDir.x < 0 )
+        {
+            render.flipX = true;
+            animator.SetBool("Run", true);
+        }
+        else if ( moveDir.x > 0 )
+        {
+            render.flipX = false;
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+    }
+
+    private void OnJump( InputValue value )
+    {
+        if ( value.isPressed )
+        {
+            Jump();
+        }
+    }
+
+    private void OnLie(InputValue value)
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        if ( ( ( 1 << collision.gameObject.layer ) & groundCheckLayer ) != 0 )
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit2D( Collider2D collision )
+    {
+        if ( ( ( 1 << collision.gameObject.layer ) & groundCheckLayer ) != 0 )
+        {
+            
+        }
+    }
+}
